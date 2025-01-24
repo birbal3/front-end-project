8aeb83a2e93be7a343476cfebab518346b0502af
Birbal <119029678+birbal3@users.noreply.github.com>
2025-01-25 04:11:06 +0530

    Update Component.jsx

--- src/components/Component.jsx
+++ src/components/Component.jsx
@@ -59,7 +59,7 @@ const Component = () => {
     <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center">
       {/* Text Content */}
       <div className="md:w-1/2 mb-8 md:mb-0">
-        <h2 className="text-4xl font-bold mb-4">Manage Your Services by your Mobile Phone</h2>
+        <h2 className="text-4xl font-bold mb-4 mt-4">Manage Your Services by your Mobile Phone</h2>
         <p className="text-lg mb-6">
           Download our app to manage and track your services. Our app enables
           you to stay in touch with our experts and aids you in tracking your
